bc10d2ef8bdf4af6d0d72caf9ba5330a62841721
Duncan <duncan.hoggan@gmail.com>
2020-01-20 12:25:50 +0200

    initial commit

---
+++ TheFreelancerTheme/Manifest.cs
@@ -0,0 +1,9 @@
+using OrchardCore.DisplayManagement.Manifest;
+
+[assembly: Theme(
+    Name = "The Freelancer Theme",
+    Author = "Duncan",
+    Website = "https://nologostudios.com",
+    Version = "0.0.1",
+    Description = "An theme adapted for creative websites."
+)]
