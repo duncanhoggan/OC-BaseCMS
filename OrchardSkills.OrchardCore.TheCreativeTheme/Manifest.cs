c35da4974dc632941528c6846d6da660cc581ff8
Duncan <duncan.hoggan@gmail.com>
2020-01-20 12:27:57 +0200

    removed unneeded proj

--- OrchardSkills.OrchardCore.TheCreativeTheme/Manifest.cs
+++
@@ -1,9 +0,0 @@
-using OrchardCore.DisplayManagement.Manifest;
-
-[assembly: Theme(
-    Name = "The Orchard Skills Creative Theme",
-    Author = "Orchard Skills",
-    Website = "https://OrchardSkills.com",
-    Version = "2.0.0",
-    Description = "An Orchard Skills theme adapted for creative websites."
-)]
